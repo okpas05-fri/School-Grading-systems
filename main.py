b0d7676803558dfd1356a510b72759f6e1116e6d
okpas05-fri <ohiemi4@gmail.com>
2025-09-30 12:40:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# name = input("enter your name: ")
+# score = int(input("enter your score: "))
+# if score >= 80 and score <= 100:
+#     print(f"{name} you got an A")
+# elif score > 69 and score <= 79:
+#    print(f"{name} you got a B") 
+# elif score >= 60 and score <= 69:
+#     print(f"{name} you got a c")
+# elif score >= 45 and score <= 59:
+#     print(f"{name} you got a D") 
+# elif score >= 35 and score <= 44:
+#     print(f"{name} you got E")          
+# else:
+#     print(f"{name} you did not write this exam")
